4dad1fbc57505d54c3db1a4bcd2aecd39d684172
Pelps12 <oluwapelps@gmail.com>
2024-01-27 20:26:26 -0600

    Add react-query

---
+++ frontend/app/home.tsx
@@ -0,0 +1,101 @@
+import { Keyboard, KeyboardAvoidingView, Platform, StyleSheet, TextInput, TouchableWithoutFeedback } from 'react-native';
+
+import EditScreenInfo from '@/components/EditScreenInfo';
+import { Text, View } from '@/components/Themed';
+import BBText from '@/components/utils/BBText';
+import { useTheme } from '@react-navigation/native';
+
+export default function TabOneScreen() {
+  const theme = useTheme();
+
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      keyboardVerticalOffset={100}
+      style={[{ backgroundColor: theme.colors.background,  }, styles.container]}>
+      
+      <View style={styles.titleContainer}>
+        <BBText style={styles.title}>Enter your seat number</BBText>
+      </View>
+      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" >
+<View  style={styles.centeredComponent}>
+    <BBText style={styles.seatedText}>Seat</BBText>
+    <TextInput
+    onSubmitEditing={(e) => console.log(e.nativeEvent.text)}
+            style={styles.input}
+            placeholder="No#"
+            placeholderTextColor="#888"
+          />
+</View>
+      
+
+        
+      </View>
+    
+    </KeyboardAvoidingView>
+
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: "relative"
+  },
+
+  titleContainer: {
+    position:"absolute",
+    backgroundColor: "transparent",
+    top: "25%"
+  },
+
+  title: {
+    fontSize: 40,
+    fontWeight: '300',
+    color: "white",
+    textAlign: "center"
+
+  },
+  separator: {
+   flex: 1,
+    height: "50%",
+    backgroundColor: "white",
+    width: '100%',
+    position:"absolute",
+    top: "50%",
+    borderTopLeftRadius: 20, /* 8px */
+    borderTopRightRadius: 20, /* 8px */
+    alignItems: 'center',
+    justifyContent: 'center',
+
+  },
+
+  centeredComponent: {
+    // Adjust the styles for your centered component
+    backgroundColor: 'transparent',
+    gap: 40,
+    alignItems: 'center',
+  },
+
+  seatedText: {
+    textAlign: 'center',
+    fontSize: 64,
+    fontWeight: 'bold',
+    color: 'black', // Change the color to suit your theme
+  },
+  input: {
+    marginTop: 10,
+    padding: 10,
+    fontSize: 48,
+    width: 200,
+    borderBottomColor: "black",
+    borderBottomWidth: 4,
+
+    borderWidth: 1,
+    borderColor: 'transparent', // Set border color to transparent
+    textAlign: "center"
+  },
+});
---
+++ frontend/providers/RKProvider.tsx
@@ -0,0 +1,48 @@
+import React, { useEffect } from "react";
+import Constants from "expo-constants";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+
+
+
+
+/**
+ * Extend this function when going to production by
+ * setting the baseUrl to your production API URL.
+ */
+const getBaseUrl = () => {
+  /**
+   * Gets the IP address of your host-machine. If it cannot automatically find it,
+   * you'll have to manually set it. NOTE: Port 3000 should work for most but confirm
+   * you don't have anything else running on it, or you'd have to change it.
+   *
+   * **NOTE**: This is only for development. In production, you'll want to set the
+   * baseUrl to your production API URL.
+   */
+  const localhost = Constants.manifest?.debuggerHost?.split(":")[0];
+
+  if (!localhost) {
+    return "";
+  }
+  return ""
+};
+
+/**
+ * A wrapper for your app that provides the TRPC context.
+ * Use only in _app.tsx
+ */
+export const RKProvider: React.FC<{
+  children: React.ReactNode;
+}> = ({ children }) => {
+
+
+  const [queryClient] = React.useState(() => new QueryClient());
+ 
+  
+
+  return (
+    
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+
+  );
+}
